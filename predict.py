0741b8a3e91bfcb6b8782202f3be069e95e1a019
Kabilar Gunalan <kabi@mit.edu>
2025-10-01 15:16:59 -0500

    Add option to use MacOS GPU

--- predict.py
+++ predict.py
@@ -184,7 +184,12 @@ def main():
     args = parser.parse_args()
     
     # Setup device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     
     # Load all models
     models = []
